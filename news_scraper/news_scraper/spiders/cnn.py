4299e2168d232dac55191918c95185fd9eba2eb9
REMitchell <ryan.e.mitchell@gmail.com>
2020-10-12 18:32:36 -0400

    Adding 03_03_e

--- news_scraper/news_scraper/spiders/cnn.py
+++ news_scraper/news_scraper/spiders/cnn.py
@@ -1,17 +1,14 @@
 # -*- coding: utf-8 -*-
-from scrapy.spiders import CrawlSpider, Rule
+from scrapy.spiders import CrawlSpider, Rule, SitemapSpider
 from scrapy.linkextractors import LinkExtractor
 from news_scraper.items import NewsArticle
 
-class CnnSpider(CrawlSpider):
+class CnnSpider(SitemapSpider):
     name = 'cnn'
     allowed_domains = ['cnn.com']
-    # Articles on the front page are dynamically loaded
-    start_urls = ['https://www.cnn.com/africa']
-    # /2020/08/28/weather/rapid-fire-disasters-in-coronavirus-pandemic-weir-wxc/index.html
-    rules = [Rule(LinkExtractor(allow=r'\/2020\/[0-9][0-9]\/[0-9][0-9]\/[a-zA-Z\-]+\/[a-zA-Z\-]+\/index.html'), callback='parse_item', follow=True)]
-    
-    def parse_item(self, response):
+    sitemap_urls = ['https://www.cnn.com/sitemaps/article-2020-10.xml']
+
+    def parse(self, response):
         article = NewsArticle()
         # <script data-rh="true">
         article['url'] = response.url
